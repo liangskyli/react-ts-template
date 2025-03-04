cdb0a5e587af8507efcc21af217b9b32e3fd41ec
liangsky <640634387@qq.com>
2025-03-04 17:45:40 +0800

    feat: support electron

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist', 'dist*', 'node_modules', 'coverage'] },
+  { ignores: ['dist', 'dist*', 'node_modules', 'coverage', 'release'] },
   {
     settings: { react: { version: '19.0' } },
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import react from '@vitejs/plugin-react-swc';
+import { rmSync } from 'node:fs';
 import { resolve } from 'node:path';
 import process from 'node:process';
 import type { ConfigEnv, UserConfig } from 'vite';
 import { defineConfig, loadEnv } from 'vite';
 import checker from 'vite-plugin-checker';
+import electron from 'vite-plugin-electron/simple';
 import svgr from 'vite-plugin-svgr';
+import pkg from './package.json';
 
 const routerBase = `/sub/`;
 
-export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+export default defineConfig(({ mode, command }: ConfigEnv): UserConfig => {
+  rmSync('dist-electron', { recursive: true, force: true });
+  const isServe = command === 'serve';
+  const isBuild = command === 'build';
+  const sourcemap = isServe || !!process.env.VSCODE_DEBUG;
   // 获取`.env`环境配置文件
   const env = loadEnv(mode, process.cwd());
   const mockProxy: Required<UserConfig>['server']['proxy'] = {
@@ -34,6 +41,54 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
     plugins: [
       react(),
       svgr(),
+      electron({
+        main: {
+          // Shortcut of `build.lib.entry`
+          entry: 'electron/main/index.ts',
+          onstart(args) {
+            if (process.env.VSCODE_DEBUG) {
+              console.log(
+                /* For `.vscode/.debug.script.mjs` */ '[startup] Electron App',
+              );
+            } else {
+              args.startup();
+            }
+          },
+          vite: {
+            build: {
+              sourcemap,
+              minify: isBuild,
+              outDir: 'dist-electron/main',
+              rollupOptions: {
+                external: Object.keys(
+                  'dependencies' in pkg ? pkg.dependencies : {},
+                ),
+              },
+            },
+          },
+        },
+        preload: {
+          // Shortcut of `build.rollupOptions.input`.
+          // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
+          input: 'electron/preload/index.ts',
+          vite: {
+            build: {
+              sourcemap: sourcemap ? 'inline' : undefined, // #332
+              minify: isBuild,
+              outDir: 'dist-electron/preload',
+              rollupOptions: {
+                external: Object.keys(
+                  'dependencies' in pkg ? pkg.dependencies : {},
+                ),
+              },
+            },
+          },
+        },
+        // Ployfill the Electron and Node.js API for Renderer process.
+        // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
+        // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+        renderer: undefined,
+      }),
       // 在浏览器中直接看到上报的类型错误（更严格的类型校验）
       checker({
         typescript: true,
