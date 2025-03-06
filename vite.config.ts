75b0918d75dc0d9bc03cd03a1c428024d26be4c2
liangsky <640634387@qq.com>
2025-03-06 17:54:58 +0800

    feat: add api proxy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'node:path';
 import process from 'node:process';
-import type { ConfigEnv, UserConfig } from 'vite';
+import type { UserConfig, UserConfigFnObject } from 'vite';
 import { defineConfig, loadEnv } from 'vite';
 import checker from 'vite-plugin-checker';
 import svgr from 'vite-plugin-svgr';
 
 const routerBase = `/sub/`;
 
-export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+export const getViteConfig: UserConfigFnObject = ({ mode }) => {
   // 获取`.env`环境配置文件
   const env = loadEnv(mode, process.cwd());
   const mockProxy: Required<UserConfig>['server']['proxy'] = {
@@ -21,9 +21,8 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
   const noMockProxy: Required<UserConfig>['server']['proxy'] = {
     '/api/': {
       // 后端服务地址
-      target: 'http://localhost:3000',
+      target: env.VITE_PROXY_SERVER_URL,
       changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ''),
     },
   };
   const proxy: Required<UserConfig>['server']['proxy'] =
@@ -45,7 +44,7 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
     ],
     resolve: {
       alias: {
-        '@': resolve(import.meta.dirname, 'src'),
+        '@': resolve(__dirname, 'src'),
       },
     },
     css: {
@@ -92,4 +91,6 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
       proxy: proxy,
     },
   };
-});
+};
+
+export default defineConfig(getViteConfig);
