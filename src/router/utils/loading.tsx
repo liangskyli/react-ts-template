71aeebfbde14cea5f71373f29733f17036fa6f82
liangsky <640634387@qq.com>
2025-04-22 20:01:56 +0800

    feat: modify Loading width and height

--- src/router/utils/loading.tsx
+++ src/router/utils/loading.tsx
@@ -2,9 +2,9 @@ import Icon from '@/components/icon';
 
 const Loading = () => (
   <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-    <div className="flex h-32 w-32 flex-col items-center justify-center rounded-lg bg-mask text-white">
+    <div className="flex h-24 w-24 flex-col items-center justify-center rounded-lg bg-mask text-white">
       <Icon className="animate-spin" name="loading" />
-      <div className="mt-2 text-base">加载中...</div>
+      <div className="mt-2 text-sm">加载中...</div>
     </div>
   </div>
 );
