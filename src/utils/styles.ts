1027658b9f1ba912b2df5e4f823a98cdddae3991
liangsky <640634387@qq.com>
2025-05-06 20:10:44 +0800

    feat: add tailwind prefix config

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -2,6 +2,7 @@ import classNames from 'classnames';
 import { extendTailwindMerge } from 'tailwind-merge';
 
 const twMerge = extendTailwindMerge({
+  prefix: '',
   extend: {
     classGroups: {
       z: [{ z: ['popup', 'mask', 'toast'] }],
